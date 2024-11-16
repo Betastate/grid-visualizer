3a877d5483927e21e35a97ea9a893fb9c2893a44
Александър Димитров <betastates@gmail.com>
2024-11-16 22:43:35 +0200

    scrolling and zooming

--- main.js
+++ main.js
@@ -2,16 +2,23 @@ let canvas, ctx;
 
 let grid = []
 let cameraPos = { x: 50, y: 50 }
+const mousePos = { x: window.innerWidth / 2, y: window.innerHeight / 2 }
 const settings = {
-    cellSize: 125,
+    cellSize: 128,
     mapSize: {
         x: 100,
         y: 100,
-    }
+    },
+    scrollSpeed: 0.05,
+    scrollMargin: 30,
+    mouseWheelStrength: 0.15
 }
 const images = {
     grass: {
         url: "img/grass.png"
+    },
+    grass2: {
+        url: "img/grass2.png"
     }
 };
 const imageNames = Object.keys(images);
@@ -39,7 +46,6 @@ window.addEventListener("load", () => {
     Resize();
 });
 
-window.addEventListener("resize", Resize);
 
 function Resize() {
 
@@ -62,13 +68,50 @@ function generateMap() {
 
 function loadingDone() {
     generateMap();
+    window.addEventListener("resize", Resize);
+    window.addEventListener("mousemove", (e) => {
+        mousePos.x = e.clientX;
+        mousePos.y = e.clientY;
+    })
+    window.addEventListener("fullscreenchange", (e) => {
+        e.preventDefault();
+    })
+    addEventListener("mousewheel", (e) => {
+        const delta = e.deltaY;
+        if (delta > 0) {
+            if (settings.cellSize * (1 - settings.mouseWheelStrength) > 64) {
+                settings.cellSize *= (1 - settings.mouseWheelStrength);
+            }
+
+        }
+        else {
+            if (settings.cellSize * (1 + settings.mouseWheelStrength) < 512) {
+                settings.cellSize *= (1 + settings.mouseWheelStrength);
+            }
+        }
+    });
     Resize();
     setInterval(update, 10);
     draw();
 }
 
 function update() {
-    // cameraPos.x += 0.01;
+    // Scrolling the map
+    if (mousePos.x < settings.scrollMargin) {
+        cameraPos.x += settings.scrollSpeed * (128 / settings.cellSize);
+    }
+    else if (mousePos.x > window.innerWidth - settings.scrollMargin) {
+        cameraPos.x -= settings.scrollSpeed * (128 / settings.cellSize);
+    }
+
+    if (mousePos.y < settings.scrollMargin) {
+        cameraPos.y += settings.scrollSpeed * (128 / settings.cellSize);
+    }
+    else if (mousePos.y > window.innerHeight - settings.scrollMargin) {
+        cameraPos.y -= settings.scrollSpeed * (128 / settings.cellSize);
+    }
+
+
 }
 
 function draw() {
@@ -78,28 +121,28 @@ function draw() {
     const cameraX = parseInt(cameraPos.x);
     const cameraY = parseInt(cameraPos.y);
 
-    let iteratorStartX = cameraX - parseInt((window.innerWidth / 2) / settings.cellSize) - 1;
-    let iteratorStartY = cameraY - parseInt((window.innerHeight / 2) / settings.cellSize) - 1;
+    let iteratorStartX = cameraX - parseInt((window.innerWidth / 2) / settings.cellSize) - 5;
+    let iteratorStartY = cameraY - parseInt((window.innerHeight / 2) / settings.cellSize) - 5;
 
-    let iteratorEndX = cameraX + parseInt((window.innerWidth / 2) / settings.cellSize) + 1;
-    let iteratorEndY = cameraY + parseInt((window.innerHeight / 2) / settings.cellSize) + 1;
+    let iteratorEndX = cameraX + parseInt((window.innerWidth / 2) / settings.cellSize) + 5;
+    let iteratorEndY = cameraY + parseInt((window.innerHeight / 2) / settings.cellSize) + 5;
 
-    console.log(iteratorStartX, iteratorStartY, iteratorEndX, iteratorEndY)
-
-    for (let y = iteratorStartY; y < iteratorEndY; y++) {
-        for (let x = iteratorStartX; x < iteratorEndX; x++) {
+    for (let y = iteratorStartY; y < iteratorEndY; y += 1) {
+        if (!grid[y]) {
+            continue;
+        }
+        for (let x = iteratorStartX; x < iteratorEndX; x += 1) {
+            if (!grid[y][x]) {
+                continue;
+            }
 
             let drawPosX = (window.innerWidth / 2) - ((x - cameraPos.x) * settings.cellSize) - (settings.cellSize / 2);
-            let drawPosY = (window.innerHeight / 2) - ((x - cameraPos.x) * settings.cellSize) - (settings.cellSize / 2);
+            let drawPosY = (window.innerHeight / 2) - ((y - cameraPos.y) * settings.cellSize) - (settings.cellSize / 2);
 
 
 
             ctx.drawImage(grid[y][x].image, drawPosX, drawPosY, settings.cellSize + 2, settings.cellSize + 2);
 
-
-            ctx.fillStyle = "#fff";
-            ctx.font = "48px serif";
-            ctx.fillText("ye", drawPosX + 50, drawPosY + 50);
         }
     }
 
